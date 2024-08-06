97880541ba8269cb6b6ef3d1822ab4947a610d38
genosty <genosty@hotmail.com>
2024-08-06 10:09:39 +0200

    nothing

--- Project01/HelloWorld.cpp
+++ Project01/HelloWorld.cpp
@@ -11,6 +11,7 @@ int main (){
     typedef std::vector<std::pair<std::string, int>> pairlist_t; //_t ofr the name convention of typedefs.
     pairlist_t xxx;
 
+    using number_t = std::string;
     std::cout << "i likje it" << '\n';
     std::cout << " yeeessss" << std::endl;
     int x; //decleration
@@ -19,7 +20,9 @@ int main (){
     int age = 27;
     std:: string name = "Genotsy";
     string LastName = "lastname";
-    std::cout << "your name is " << name << " and your age is: " << age << " year old";
+    std::cout << "your name is " << name << " and your age is: " << age << " year old" <<'\n';
+    std::string stringss[]= {"hello", "hola", "comestats"};
 
+    for(std::string strings : stringss)std::cout << strings << '\n';
     return 0;
 }
